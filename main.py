76b8d4f999edadc6d1d2afc7c9837af8ff306691
MehmetOguzAlpka <91716273+MehmetOguzAlpkan@users.noreply.github.com>
2021-10-02 19:18:11 +0000

    Mehmet Oğuz Alpkan 1.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-
-print ('12345')
+import numpy
+print('log2 finder')
+a = int(input('write the value of A'))
+b = int(input('write the value of B'))
+d = numpy.log2(b)
+c = a**b
+print('a**b: ', c,  "- log:", d )
